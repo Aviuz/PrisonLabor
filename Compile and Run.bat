c68d86d16b4447c6ff9047bebf7f9fe7b713dd40
Avius <lion29.box@gmail.com>
2018-01-16 02:59:55 +0100

    Updated .bat files
    - updated directories for new computer

--- Compile and Run.bat
+++ Compile and Run.bat
@@ -1,36 +1,36 @@
 :: ========= Build ==========
 
-devenv "D:\Git Repository\Prison Labor Mod\Source\PrisonLabor.sln" /build Debug
+devenv "Source\PrisonLabor.sln" /build Debug
 
 
 
 :: ========= Copy ==========
  
-rd "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\PrisonLabor" /s /q
-mkdir "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\PrisonLabor"
+rd "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\PrisonLabor" /s /q
+mkdir "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\PrisonLabor"
 
 :: About
-mkdir "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\PrisonLabor\About"
-xcopy "D:\Git Repository\Prison Labor Mod\About\*.*" "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\PrisonLabor\About" /e
+mkdir "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\PrisonLabor\About"
+xcopy "About\*.*" "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\PrisonLabor\About" /e
 
 :: Assemblies
-mkdir "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\PrisonLabor\Assemblies"
-xcopy "D:\Git Repository\Prison Labor Mod\Assemblies\*.*" "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\PrisonLabor\Assemblies" /e
+mkdir "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\PrisonLabor\Assemblies"
+xcopy "Assemblies\*.*" "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\PrisonLabor\Assemblies" /e
 
 :: Defs 
-mkdir "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\PrisonLabor\Defs"
-xcopy "D:\Git Repository\Prison Labor Mod\Defs\*.*" "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\PrisonLabor\Defs" /e
+mkdir "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\PrisonLabor\Defs"
+xcopy "Defs\*.*" "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\PrisonLabor\Defs" /e
 
 :: Languages
-mkdir "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\PrisonLabor\Languages"
-xcopy "D:\Git Repository\Prison Labor Mod\Languages\*.*" "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\PrisonLabor\Languages" /e
+mkdir "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\PrisonLabor\Languages"
+xcopy "Languages\*.*" "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\PrisonLabor\Languages" /e
 
 :: Textures
-mkdir "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\PrisonLabor\Textures"
-xcopy "D:\Git Repository\Prison Labor Mod\Textures\*.*" "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\PrisonLabor\Textures" /e
+mkdir "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\PrisonLabor\Textures"
+xcopy "Textures\*.*" "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\PrisonLabor\Textures" /e
 
 :: changelog.txt
-copy "D:\Git Repository\Prison Labor Mod\changelog.txt" "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\PrisonLabor\changelog.txt"
+copy "changelog.txt" "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\PrisonLabor\changelog.txt"
 
 
 
