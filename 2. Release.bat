4e8801a89876582386e73fceffe78d2441bae100
Aviuz <avius.ravis@gmail.com>
2019-10-27 22:09:23 +0100

    updated bat file

--- 2. Release.bat
+++ 2. Release.bat
@@ -2,12 +2,12 @@
 
 SET mod_name=PrisonLabor
 SET github_release=https://github.com/Aviuz/PrisonLabor/releases
-SET steam_changelog=https://steamcommunity.com/sharedfiles/filedetails/changelog/972057888
-SET steam_description=https://steamcommunity.com/sharedfiles/itemedittext/?id=972057888
+SET steam_changelog=https://steamcommunity.com/sharedfiles/filedetails/changelog/1899474310
+SET steam_description=https://steamcommunity.com/sharedfiles/itemedittext/?id=1899474310
 SET ludeon_thread=https://ludeon.com/forums/index.php?topic=34465.0
 
 SET target_directory=D:\Program Files\Steam\steamapps\common\RimWorld\Mods\%mod_name%
-SET zip_directory=C:\Users\avius\Desktop\%mod_name%.zip
+SET zip_directory=C:\Users\avius\Desktop\%mod_name%.rar
 
 
 :: ========= Zip archive ==========
