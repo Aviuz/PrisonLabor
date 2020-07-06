780878623890efc4119e5c64184ee10e943ef36a
Hazzer <warlocksforge@gmail.com>
2020-07-06 17:16:25 +0200

    Version 1.1.10

---
+++ Source/HarmonyPatches/Patches_Construction/Patch_Smoothing.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+
+namespace PrisonLabor.HarmonyPatches.Patches_Construction
+{
+    [HarmonyPatch()]
+    class Patch_Smoothing
+    {
+        static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return (MethodBase)typeof(WorkGiver_ConstructSmoothWall).GetMethod("HasJobOnCell");
+            yield return (MethodBase)typeof(WorkGiver_ConstructAffectFloor).GetMethod("HasJobOnCell");
+        }
+
+        public static bool Postfix(bool __result, Pawn pawn, IntVec3 c)
+        {
+            if(__result && pawn.IsPrisonerOfColony)
+            {
+               return pawn.CanReach(c, PathEndMode.Touch, pawn.NormalMaxDanger());
+            }
+
+            return __result;
+        }
+    }
+}
