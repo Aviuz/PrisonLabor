5bf420fa735db35fea24ef066780d684224d1294
Karim Batbouta <kbatbouta66@gmail.com>
2020-07-08 11:09:19 -0700

    ...

--- Source/PrisonLaborTools/UI/TabPrisonLabor.cs
+++ Source/PrisonLaborTools/UI/TabPrisonLabor.cs
@@ -44,6 +44,8 @@ namespace PrisonLaborDebug.UI
                 return;
 
             var yOffset = 10f;
+            var tyOffset = yOffset;
+
             Widgets.Label(new Rect(10, yOffset, 200, 35), "Wardens in the room " + Tracked.Wardens[roomID].Count);
             yOffset += 35;
 
@@ -54,27 +56,81 @@ namespace PrisonLaborDebug.UI
             Widgets.DrawLine(new Vector2(10, yOffset), new Vector2(200, yOffset), color: Color.white, 2f);
             yOffset += 5;
 
+            tyOffset = yOffset;
             if (jobHistory.Count != 0)
             {
-                yOffset += 10;
+                Text.Font = GameFont.Tiny;
 
+                yOffset += 10;
                 if (((Pawn)comp.parent).CurJob != null)
                 {
                     if (((Pawn)comp.parent).CurJobDef != jobHistory.Last())
                         jobHistory.Add(((Pawn)comp.parent).CurJobDef);
                 }
 
-                Widgets.Label(new Rect(10, yOffset, 200, 20), "Pawn cur JobDef " + jobHistory.Last());
-                yOffset += 20;
+                try
+                {
+                    Widgets.Label(new Rect(10, yOffset, 400, 20), "Pawn cur JobDef " + jobHistory.Last());
+                    yOffset += 15;
+                }
+                catch
+                {
+                    Widgets.Label(new Rect(10, yOffset, 400, 20), "Pawn cur JobDef None");
+                    yOffset += 15;
+                }
+
+                try
+                {
+                    Widgets.Label(new Rect(10, yOffset, 400, 20), "Pawn cur job Lord " + ((Pawn)comp.parent).jobs.curJob.lord.ToString());
+                    yOffset += 15;
+                }
+                catch
+                {
+                    Widgets.Label(new Rect(10, yOffset, 400, 20), "Pawn cur job Lord None");
+                    yOffset += 15;
+                }
+
+                try
+                {
+                    Widgets.Label(new Rect(10, yOffset, 400, 20), "Pawn cur Think tree name" + ((Pawn)comp.parent).jobs.curJob.jobGiverThinkTree);
+                    yOffset += 15;
+
+                    Widgets.Label(new Rect(10, yOffset, 400, 20), "Pawn cur Think tree root" + ((Pawn)comp.parent).jobs.curJob.jobGiverThinkTree.thinkRoot);
+                    yOffset += 15;
+                }
+                catch
+                {
+                    Widgets.Label(new Rect(10, yOffset, 400, 20), "Pawn cur Think tree name None");
+                    yOffset += 15;
+                }
+
+                try
+                {
+                    Widgets.Label(new Rect(10, yOffset, 400, 20), "Pawn cur Toil" + ((Pawn)comp.parent).jobs.curDriver.CurToil.ToString());
+                    yOffset += 15;
+                }
+                catch
+                {
+                    Widgets.Label(new Rect(10, yOffset, 400, 20), "Pawn cur Toil None");
+                    yOffset += 15;
+                }
+
+                var tdriver = ((Pawn)comp.parent).jobs.curDriver;
+                if (tdriver != null)
+                {
+                    Widgets.Label(new Rect(10, yOffset, 400, 20), "Pawn JobDriver" + tdriver.ToString());
+                    yOffset += 15;
+                }
+
+                yOffset += 25;
 
                 for (int i = 0; i < jobHistory.Count - 1; i++)
                 {
-                    Widgets.Label(new Rect(10, yOffset, 200, 20), -(i + 1) + " " + jobHistory[jobHistory.Count - i - 2]);
+                    Widgets.Label(new Rect(10, yOffset, 400, 20), -(i + 1) + " " + jobHistory[jobHistory.Count - i - 2]);
                     yOffset += 20;
                 }
 
-                if (jobHistory.Count >= 5) { jobHistory.RemoveRange(5, jobHistory.Count - 5); }
-
+                if (jobHistory.Count >= 5) { jobHistory.RemoveRange(0, jobHistory.Count - 5); }
                 yOffset += 10;
 
                 // A line divider
@@ -86,6 +142,8 @@ namespace PrisonLaborDebug.UI
                 if (((Pawn)comp.parent).CurJob != null)
                     jobHistory.Add(((Pawn)comp.parent).CurJobDef);
             }
+
+            Text.Font = GameFont.Small;
         }
 
         private static Vector2 nodeScrollPosition = Vector2.zero;
