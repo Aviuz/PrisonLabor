461a83150d5e83b90c8b94f11164803f7af46daa
Hazzer <warlocksforge@gmail.com>
2022-10-28 12:13:41 +0200

    fixes

--- Source/Core/Meta/VersionUtility.cs
+++ Source/Core/Meta/VersionUtility.cs
@@ -5,7 +5,7 @@ namespace PrisonLabor.Core.Meta
 {
     public class VersionUtility
     {
-        public const Version versionNumber = Version.v1_4_0;
+        public const Version versionNumber = Version.v1_4_1;
         public const string versionString = "1.4.1";
 
         public static Version VersionOfSaveFile { get; set; }
