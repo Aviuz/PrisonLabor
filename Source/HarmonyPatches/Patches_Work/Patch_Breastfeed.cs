9b4c1233c1ca583ad71c332d9a08864793acb52a
Hazzer <warlocksforge@gmail.com>
2025-08-17 21:04:46 +0200

    1.6.3

--- Source/HarmonyPatches/Patches_Work/Patch_Breastfeed.cs
+++ Source/HarmonyPatches/Patches_Work/Patch_Breastfeed.cs
@@ -2,13 +2,6 @@
 using PrisonLabor.Core;
 using PrisonLabor.Core.Other;
 using RimWorld;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Verse;
 
 namespace PrisonLabor.HarmonyPatches.Patches_Work
@@ -20,11 +13,11 @@ namespace PrisonLabor.HarmonyPatches.Patches_Work
     [HarmonyPatch("HasBreastfeedCompatibleFactions")]
     static bool Postfix(bool __result, Pawn mom, Pawn baby)
     {
-      if (mom.IsPrisonerOfColony || baby.IsPrisonerOfColony)
+      if (__result)
       {
-        return ChildcareUtility.HasBreastfeedCompatibleFactions(PrisonLaborUtility.GetPawnFaction(mom), baby);
+        return true;
       }
-      return __result;
+      return mom.IsPrisonerOfColony && ChildcareUtility.HasBreastfeedCompatibleFactions(PrisonLaborUtility.GetPawnFaction(mom), baby);
     }
   }
 }
